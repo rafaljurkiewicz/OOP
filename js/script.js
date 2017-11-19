ca3af2a6d42b596855d51c7c1ea6e0d3439de71e
Rafal Jurkiewicz <rafal.jurkiewicz87@gmail.com>
2017-11-19 21:42:59 +0100

    task 11.5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
@@ -14,3 +15,24 @@ var OnePlusOne = new Phone("OnePlus One", 1250, "black");
 iPhone6S.printInfo();
 SamsungGalaxyS6.printInfo();
 OnePlusOne.printInfo();
+*/
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
+}
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
